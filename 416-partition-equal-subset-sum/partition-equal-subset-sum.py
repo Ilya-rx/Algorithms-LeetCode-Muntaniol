7c9680427f2149424353dbcae56377ef4099743f
Ilya-rx <Ilya.muntaniol.07@bk.ru>
2026-04-22 13:34:00 +0500

    Time: 650 ms (62.53%) | Memory: 19 MB (99.27%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canPartition(self, nums):
+        total = sum(nums)
+        
+        # если сумма нечетная
+        if total % 2 != 0:
+            return False
+        
+        target = total // 2
+        
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        
+        return dp[target]
